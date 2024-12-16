298113f369e6a290ddd6e93323de0b3f2b36fecc
randukelvin <randukelvin@gmail.com>
2024-12-16 12:38:04 +0300

    added card component

---
+++ README.md
@@ -0,0 +1,50 @@
+# React Card
+
+This is a card template library designed with [React](https://react.dev)  and  [Chakra ui](https://www.chakra-ui.com) .
+## Prerequite
+[Chakra ui](https://www.chakra-ui.com)
+[React](https://react.dev) 
+
+## Installation
+
+
+
+    npm i @randukelvin/card
+    yarn add @randukelvin/card
+
+
+## Usage
+
+
+You can use it as follows:
+
+
+
+```js
+
+import { Card } from '@randukelvin/card'
+
+function App() {
+
+
+  return <Card name='Chill Spot' src='https://bit.ly/2Z4KKcF' alt='Rear view of modern home' badge='restaurants' rating={5} reviewCount={34} />
+}
+
+export default App
+
+
+
+
+```
+
+## Props
+
+| Props        | Type        | Description |
+| ------------- |:-------------:| -----:|
+| name   |   string  |  name of product      | 
+| src   |  string   |  image source      | 
+| badge   |  string   |  string to be shown in badge    | 
+| alt   |   string  |  alt string of image    | 
+| distance   | string    |  distance i.e 1km away      | 
+| rating   | number    |  1-5     | 
+| reviewCount   | number    |  review number    | 
---
+++ src/components/Card.tsx
@@ -0,0 +1,67 @@
+import { StarIcon } from '@chakra-ui/icons'
+import { Box, Image, Badge,Icon } from '@chakra-ui/react'
+import { FaBeer } from "react-icons/fa";
+
+type Iprops = {
+    name?: string;
+    src?: string;
+    badge?: string;
+    alt?: string;
+    distance?: string;
+    rating?: number;
+    reviewCount?: number;
+};
+export function Card(props: Iprops) {
+
+    const { name, src, alt, badge, distance, rating, reviewCount } = props
+
+
+
+    return (
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <Image h={100} width={'100%'} src={src ? src : 'https://bit.ly/2Z4KKcF'} alt={alt ? alt : 'Rear view of modern home with pool'} />
+
+            <Box p='4'>
+                <Box display='flex' alignItems='baseline'>
+                    <Badge borderRadius='full' px='2' colorScheme='teal.500'>
+                        {badge ? badge : 'restaurants'}
+                    </Badge>
+
+                </Box>
+
+                <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h4'
+                    lineHeight='tight'
+
+                >
+                    {name ? name : 'Chill Spot'}
+                </Box>
+
+                <Box>
+
+                    <Box as='span' fontSize='xs' color='gray.600' >
+                        {distance ? distance : '1 km away'}
+                    </Box>
+                </Box>
+
+                <Box display='flex' mt='2' alignItems='center'>
+                  
+                    {Array(5)
+              .fill('')
+              .map((_, i) => (
+               
+                <Icon fontSize="12px"   color={i < rating! ? 'teal.500' : 'gray.300'}>
+                  <FaBeer />
+              </Icon>
+              
+              ))}
+                    <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                        {reviewCount ? reviewCount : 34} reviews
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
